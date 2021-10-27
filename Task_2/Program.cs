825112d5ecd169baa60650d3862b2990b9da97df
DmitriyLosev <dmitriy.losev@icloud.com>
2021-10-27 16:39:34 +0300

    added Task 2 comment

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Введите своё имя ");
-string username = Console.ReadLine();
+﻿Console.Write("Введите своё имя "); //Write без Line позволяет не переносить слова на другую строку
+string username = Console.ReadLine(); //string переменная (тип данных) где храняться строки
 Console.Write("Привет, ");
 Console.Write(username);
