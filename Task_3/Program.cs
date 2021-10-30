87b991a88c7766f73522ebe0fba37ddff3034620
DmitriyLosev <dmitriy.losev@icloud.com>
2021-10-30 08:12:02 +0300

    correct Test 3 and friends and dog

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 35; //int тип данных для хранения целых чисел
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); //int тип данных для хранения целых чисел. После знака равно оператор генератор псевдо случайных чисел от 1 до 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
