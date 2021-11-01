f3db182a486537feca242189e022b11abffa5cf1
DmitriyLosev <dmitriy.losev@icloud.com>
2021-11-01 11:15:30 +0300

    add Task_Del

---
+++ Task_Del/Program.cs
@@ -0,0 +1,18 @@
+﻿long index = 2;
+long num = 600851475143;
+while (index * index < num)
+{
+    if(num % index == 0 )
+    {
+        num = num / index;
+    Console.WriteLine($"Остаок после деления {num}");
+    Console.WriteLine($"делитель {index}");
+    }
+    else 
+    {
+        index++;
+    }
+}
+
+Console.WriteLine($"Наибольший простой делитель делитель равен {num}");
+  
