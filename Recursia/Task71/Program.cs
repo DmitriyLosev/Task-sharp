5e044f1c2f7b00431fe9ccbdf7715e0e2470ece0
DmitriyLosev <dmitriy.losev@icloud.com>
2021-12-05 11:23:38 +0300

    Task71

--- Recursia/Task71/Program.cs
+++ Recursia/Task71/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу вычисления функции Аккермана
+//             n + 1, m = 0
+// A(m,n) =    A(m - 1, 1) m > 0, n = 0;
+//             A(m - 1, A(m, n - 1)) , m > 0, n > 0
+int m = 4;
+int n = 0;
+int FunctionAkkerman(int m, int n)
+{
+    if (m == 0) return  (n + 1);
+    if (m > 0 && n == 0) return FunctionAkkerman(m - 1, 1);
+    return FunctionAkkerman(m - 1, FunctionAkkerman(m, n - 1 ));
+}
+Console.WriteLine(FunctionAkkerman(m, n));
