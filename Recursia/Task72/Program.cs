2937efb2e353069df3fa52c595d943bc367e357e
DmitriyLosev <dmitriy.losev@icloud.com>
2021-12-05 13:03:57 +0300

    Task72

---
+++ Recursia/Task72/Program.cs
@@ -0,0 +1,8 @@
+﻿// Написать программу возведения числа А в целую стень B
+int A = 2;
+int B = 3;
+int Exponentiation(int A, int B)
+{
+    return B == 0 ? 1 : Exponentiation(A, B - 1) * A;
+}
+Console.WriteLine(Exponentiation(A, B));
